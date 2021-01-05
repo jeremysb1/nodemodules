3cd7129334977129ec5a94c25582f65d5aeda810
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-01-05 15:59:46 -0500

    basic concept of Middleware

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+/* learning basic concept of Middleware */
+
+app.use((req, res, next) => {
+	console.log('Hi');
+	next();
+});
+
 app.get('/', (req, res) => {
 	res.send('Yo');
 });
